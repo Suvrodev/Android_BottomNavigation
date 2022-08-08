419cc304375d1c8c08c633a604f4f2e1de10bea5
Suvrodeb <suvrodevhowlader1408@gmail.com>
2022-08-09 01:32:48 +0600

    pass data fragment to fragment

--- BottomNavigation/app/src/main/java/com/example/bottomnav/HomeFragment.java
+++ BottomNavigation/app/src/main/java/com/example/bottomnav/HomeFragment.java
@@ -1,27 +1,69 @@
 package com.example.bottomnav;
 
+import android.animation.TimeAnimator;
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class HomeFragment extends Fragment {
 
-
+    View view;
+    EditText GetTeext;
+    Button SendButton;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        view=inflater.inflate(R.layout.fragment_home, container, false);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        GetTeext= view.findViewById(R.id.ed_id);
+        SendButton=view.findViewById(R.id.btn);
+
+
+        SendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str=GetTeext.getText().toString();
+                Toast.makeText(getContext(),str, Toast.LENGTH_SHORT).show();
+
+                Bundle bundle=new Bundle();
+                bundle.putString("strr",str);
+
+               // MessageFragment messageFragment=new MessageFragment();
+                //messageFragment.setArguments(bundle);
+
+
+               ///Lagbe->
+              //  getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.framelayout_id,messageFragment).commit();
+
+                /////////////////////////
+                getParentFragmentManager().setFragmentResult("okk",bundle);
+
+
+               // f2.setT(str);
+            }
+        });
+
+
+        return view ;
     }
 }
--- BottomNavigation/app/src/main/java/com/example/bottomnav/MessageFragment.java
+++ BottomNavigation/app/src/main/java/com/example/bottomnav/MessageFragment.java
@@ -2,16 +2,21 @@ package com.example.bottomnav;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentResultListener;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MessageFragment extends Fragment {
 
 
+    View view;
 
     public MessageFragment() {
         // Required empty public constructor
@@ -21,7 +26,28 @@ public class MessageFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        view=inflater.inflate(R.layout.fragment_message, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message, container, false);
+
+        TextView textView=view.findViewById(R.id.tv);
+
+//        Bundle bundle=getArguments();
+//        try {
+//            String str = bundle.getString("strr");
+//        }catch (Exception e){
+//            Toast.makeText(getContext(),e.toString(),Toast.LENGTH_SHORT).show();
+//        }
+//        textView.setText(str);
+
+        //new likhe FragmentResult likhlei sob chole asbe
+        getParentFragmentManager().setFragmentResultListener("okk", this, new FragmentResultListener() {
+            @Override
+            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+                String data=result.getString("strr");
+                textView.setText(data);
+                Toast.makeText(getContext(),data,Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
     }
 }
